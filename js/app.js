497d2853e2e833da8eef6f13db0748b8618a6331
Ray Estrada <ray.estrada@agencychief.com>
2018-10-01 16:49:04 -0700

    Vuetro.0.2 load an array of data with a for loop First method

--- js/app.js
+++ js/app.js
@@ -1,8 +1,38 @@
+// Array of tasks
+var tasksdata = [
+  {
+    title: 'Load the washer',
+    time: 0.25
+  },
+  {
+    title: 'Move clothes to the dryer',
+    time: 0.5
+  },
+  {
+    title: 'Empty dryer and fold clothes',
+    time: 1
+  }
+];
+
 // Initalizes the vue instance and properties
 var app = new Vue({
   el: '#app',
 
   data: {
-    hello: 'Hello World!'
+    hello: 'Hello World!',
+    tasks: tasksdata
+  },
+
+  methods: {
+    // Calculates total time from tasks object property
+    totalTime: function (tasks) {
+      var total = 0;
+      var i;
+
+      for (i = 0; i < tasks.length; i++) {
+        total = total + tasks[i].time;
+      }
+      return total;
+    }
   }
 });
